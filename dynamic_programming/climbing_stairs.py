cc396299a3141dea278a83c4a8f91b5515ea14be
Clement <clement7903@hotmail.com>
2026-01-14 17:28:51 +0800

    update code to use the dictionary as a cache

--- dynamic_programming/climbing_stairs.py
+++ dynamic_programming/climbing_stairs.py
@@ -1,7 +1,7 @@
 """
 Given an input integer n, count the number of distinct ways to climb to n using just 1 or 2 steps each time.
 """
-def climbStairs(n: int) -> int: 
+def climbStairs(n: int, cache = {1: 1,2: 2}) -> int: 
     """
     Brute force approach is to iterate through all possible unique combinations of 1s & 2s until the floor is reached. There are 2 possible ways to solve:
     a. Use n as the starting value, keep deducting n until reaches 0. Increase the counter by 1. Repeat this loop... This will be too slow!
@@ -24,27 +24,12 @@ def climbStairs(n: int) -> int:
 
         n = 4:
         => if 1st step taken = 1, then the number of ways to climb remainder 3 is climbStairs(3). if 1st step taken = 2, then number of ways to climb remainder 2 is climbStairs(2).
-
-
     """
-    if n == 1:
-        return 1
-    elif n == 2:
-        return 2
-    
-    cache = {
-        1: 1,
-        2: 2
-             }
+    if n in cache:
+        return cache[n]
+    else:
+        cache[n] = climbStairs(n-1) +climbStairs(n-2)
 
-    result1 = cache.get(n-1, climbStairs(n-1))
-    result2 = cache.get(n-2, climbStairs(n-2))
-    
-    return result1 + result2
-
-
-    """
-    Dynamic programming method is to recognise that the num. of unique combinations for a lower floor (e.g. Floor #3) could be used for a higher floor (Floor #15). Hence, we could save the results for the lower floor and use it to "skip" the calculation for the lower floors
-    """
+    return climbStairs(n)
 
 print(climbStairs(45))
